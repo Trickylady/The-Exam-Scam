623e6ad8cf9f98d7bf0eef5c4a919f0d7efe5ddc
Dario De Vita <devitadario@gmail.com>
2025-09-11 11:38:55 +0200

    score global updated

--- instances/score_recap.gd
+++ instances/score_recap.gd
@@ -6,4 +6,15 @@ func _ready() -> void:
 
 
 func _populate() -> void:
-	$lb_score.text = "[wave]%s[/wave]" % Mng.stats.get_global_scores()
+	var text: String
+	text = "Total score %s" % stat_value(Mng.stats.get_global_scores(), Color.GREEN)
+	text += "\nCuts %s" % stat_value(Mng.stats.get_total_cuts())
+	text += "\nHits %s" % stat_value(Mng.stats.get_total_hits(), Color.ORANGE_RED)
+	text += "\nPowerups %s" % stat_value(Mng.stats.get_total_powerups())
+	text += "\nTotal time %s" % stat_value(GameStats.t_msec_to_string(Mng.stats.get_total_time()))
+	$lb_score.text = text
+
+
+func stat_value(value: Variant, col: Color = Color.GOLD, text_size: int = 50) -> String:
+	var col_hex: String = col.to_html(false)
+	return "[color=%s][font_size=%s][wave]%s[/wave][/font_size][/color]" % [col_hex, text_size, value]
